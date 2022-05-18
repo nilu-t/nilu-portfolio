92dcba779497b44ddbd9d2367751ebb070d75c90
Nilushanth Thiruchelvam <nilu.thiru@gmail.com>
2022-05-17 22:15:07 -0400

    added hulk smash animation in home page.

--- static/scripts/nilu.js
+++ static/scripts/nilu.js
@@ -11,7 +11,7 @@ const terminal_words = [
     "or you can use the terminal.",
 
     /** Programming lanuages I know */
-    "I'm skilled in ",
+    "I'm skilled in...",
     "Java development",
     "Bash scripting",
     "Javascript development",
@@ -19,13 +19,14 @@ const terminal_words = [
     "C programming",
 
     /** Soft skills I have */
-    "I also have skills in",
+    "I also have skills in...",
     "Creativity",
     "Problem-solving",
     "Time management",
-    "",
-    "",
-    ""
+
+    /** Telling them to check out the rest of the site */
+    "Check out the rest of the site.",
+    "Have fun (^-^)/"
 ];
 
 var delaySpeed = 40;
@@ -33,11 +34,17 @@ var delaySpeed = 40;
 var row = 0;
 var col = 0;
 
-function clear(){
+/**
+ * Helper function used for clearing and executing typeEachCharacter with delay.
+ */
+ function clear(){
     document.getElementById("full_name").innerHTML = "&nbsp"; //clear the inner HTML and a non-breaking space is added to the HTML.
     setTimeout(typeEachCharacter, delaySpeed); //type the next character with delayed speed.
 }
 
+/**
+ * Function used for typing each terminal_words string character by character on the homepage.
+ */
 function typeEachCharacter(){
 
     if (col == terminal_words[row].length){
